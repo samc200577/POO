e05c6460295501d487a91eb6a336598530b20311
samc200577 <samuel.calderonc@uqvirtual.edu.co>
2025-02-10 14:41:45 -0500

    create proyect

---
+++ calculadora/src/main/java/co/edu/uniquindio/poo/Calculadora.java
@@ -0,0 +1,41 @@
+package co.edu.uniquindio.poo;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Calculadora {
+
+    public static int sumar(int valor1, int valor2){
+        int resultado = valor1 + valor2;
+        return resultado;
+    }
+
+    public static int restar(int valor1, int valor2){
+        int resultado = valor1 - valor2;
+        return resultado;
+    }
+
+    public static int multiplicar(int valor1, int valor2){
+        int resultado = valor1 * valor2;
+        return resultado;
+    }
+
+    public static int dividir(int valor1, int valor2){
+        int resultado = valor1 / valor2;
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el valor 1: ");
+        int valor1 = scanner.nextInt();
+        System.out.println("Ingrese el valor 2: ");
+        int valor2 = scanner.nextInt();
+
+        System.out.println("El valor de operación suma es: " + sumar(valor1,valor2));
+        System.out.println("El valor de operación multiplicacion es: " + multiplicar(valor1,valor2));
+        System.out.println("El valor de operación resta es: " + restar(valor1,valor2));
+        System.out.println("El valor de operación dividir es: " + dividir(valor1,valor2));
+    }
+}
